e2b7e853927a85afb1ee5e33d951fe7e2cf405cd
NeedRam <rajaropen@gmail.com>
2026-02-02 21:48:22 +0000

    add views to init

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -1,10 +1,11 @@
-# blue prints are imported 
+# blue prints are imported
 # explicitly instead of using *
 from .user import user_views
 from .index import index_views
 from .auth import auth_views
+from .ai_portal import ai_portal_views
 from .admin import setup_admin
 
 
-views = [user_views, index_views, auth_views] 
+views = [user_views, index_views, auth_views, ai_portal_views]
 # blueprints must be added to this list
